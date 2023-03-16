6ec4cfcd274607bea287c9e24feebe49df67582b
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-15 21:36:50 -0300

    refac: add new stacks for auth and application pages

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native';
-
+import { NavigationContainer, TabRouter } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './src/screens/Home'
 import SignIn from './src/screens/SignIn';
+import Preload from './src/screens/Preload';
+import SignUp from './src/screens/SignUp';
+import Cursos from './src/screens/Cursos';
 
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const AppStack = () => (
+  <Tab.Navigator initialRouteName='Home'
+    screenOptions={{headerShown: false}}
+  >
+    <Tab.Screen name="Home" component={Home}/>
+    <Tab.Screen name="Cursos" component={Cursos}/>
+  </Tab.Navigator>
+)
+
+const AuthStack = () => (
+  <Stack.Navigator initialRouteName='SignIn'>
+    <Stack.Screen name="Preload" component={Preload} />
+    <Stack.Screen name="SignIn" component={SignIn} />
+    <Stack.Screen name="SignUp" component={SignUp} />
+  </Stack.Navigator>
+)
 
 const App = ({ navigation }) => {
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='SignIn'>
-        <Stack.Screen name="SignIn" component={SignIn}/>
-        <Stack.Screen name="Home" component={Home}/>
+      <Stack.Navigator initialRouteName='AuthStack' 
+        screenOptions={{headerShown: false}}
+      >
+        <Stack.Screen component={AuthStack} name="AuthStack" />
+        <Stack.Screen component={AppStack} name="AppStack" />
       </Stack.Navigator>
     </NavigationContainer>
   )
