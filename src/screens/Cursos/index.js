f2f9ad0cb3ef92a8b9ab15aa090e00b749ca08c6
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-15 21:45:56 -0300

    feat: add Cursos screen

---
+++ src/screens/Cursos/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Cursos = () => {
+    return (
+        <View>
+            <Text>Cursos</Text>
+        </View>
+    )
+}
+
+export default Cursos
