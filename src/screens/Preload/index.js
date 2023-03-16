0bd47205f4e9ef542fd4f3fcf4e9cf1b14b38f29
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-15 21:46:21 -0300

    feat: add Preload screen

---
+++ src/screens/Preload/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Preload = () => {
+    return (
+        <View>
+            <Text>Preload</Text>
+        </View>
+    )
+}
+
+export default Preload
