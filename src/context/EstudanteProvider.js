523e0e3c54d9b81aab4385eb104fe10fce042c2b
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-31 08:56:17 -0300

    feat: adicionado context Estudante Provider

---
+++ src/context/EstudanteProvider.js
@@ -0,0 +1,24 @@
+import React, {createContext, useEffect} from 'react';
+import firestore from '@react-native-firebase/firestore';
+
+export const EstudanteContext = createContext({})
+
+export const EstudanteProvider = ({children}) => {
+
+    useEffect(() => {
+        const listener = firestore()
+            .collection('estudantes')
+            .orderBy('nome')
+            .onSnapshot((snapshot) => {
+                console.log(snapshot)
+            })
+
+        return () =>{listener()}
+    }, [])
+
+    return (
+        <EstudanteContext.Provider value={{}}>
+            {children}
+        </EstudanteContext.Provider>
+    )
+}
